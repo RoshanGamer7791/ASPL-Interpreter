ee3b395e1e47b323cf981bf1d19d7f35e1cf2d43
Roshan <mdtahanchy@gmail.com>
2026-03-23 18:08:23 +0000

    Made git ignore the testing stuff

--- file.py
+++
@@ -1,7 +0,0 @@
-from asplinterpreter import *
-SQUARE(10, 10, 110, 110, "red")
-TRI(100, 100, 150, 25, 200, 100, "yellow")
-LINE(0, 0, 800, 600, "white", 10)
-BGCOLOR("#1e1e1e")
-PIXEL(30, 30, "green")
-PRINT("Hello World!")
